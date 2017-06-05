5c2601b0dfb90bcef8c949306f696c3d170643a7
SidRoy10 <siddhartha.a.roy@capgemini.com>
2017-06-05 17:02:20 +0530

    Add files via upload

---
+++ DevOps1/src/java1/Demo.java
@@ -0,0 +1,17 @@
+package java1;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+
+		int a=10;
+		int b=20;
+		int c;
+		c= a+b;
+		System.out.println("Answer is:"+c);
+
+
+	}
+
+}
